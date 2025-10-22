70f210fe2a2c3fcf2ce6d6c05de09cddac03ea9c
Evan Whelan <41494790+evaan@users.noreply.github.com>
2025-10-22 12:53:26 -0230

    I think this is the correct firmware?

--- Profiler/src/main.cpp
+++ Profiler/src/main.cpp
@@ -3,8 +3,6 @@
 #include <WiFi.h>
 #include <Wire.h>
 #include <MS5837.h>
-#include <ElegantOTA.h>
-#include <ESPAsyncWebServer.h>
 #include <Array.h>
 
 #define SDAPIN 22
@@ -13,11 +11,18 @@
 //https://www.ti.com/lit/ds/symlink/drv8231a.pdf page 11 for dc
 #define IN1PIN 14
 #define IN2PIN 25
-#define BUTTONPIN 27
+#define BOTTOMPIN 27
+#define TOPPIN 26 //TODO: needs to be set
 #define TIMEOUT 19000
-#define SINKTIME 60000
 
-const char companyNumber[5] = {'E', 'X', '2', '6', '\0'};
+#define CHANNEL_IN1 0
+#define CHANNEL_IN2 1
+#define PWM_FREQ 1000
+#define PWM_RES 8
+
+#define POT_PIN 35
+
+const char companyNumber[5] = {'E', 'X', '0', '6', '\0'}; //TODO: this will need to be changed
 
 typedef struct struct_message {
   uint8_t mod = 1;
@@ -51,7 +56,7 @@ esp_now_peer_info_t peerInfo;
 
 MS5837 sensor;
 
-AsyncWebServer server(80);
+// AsyncWebServer server(80);
 
 bool profiling = false;
 bool priming = false;
@@ -69,14 +74,19 @@ float totalDepth = 0;
 float totalPressure = 0;
 int profiles = 0;
 
+int timeInRegion = 0;
+
 void getDepthData(void * parameter) {
   for(;;) {
     if (profiling) {
-      depthQueue.push_back({millis(), sensor.depth(), sensor.pressure()/10});
-    } else if (!profiling && (totalDepth != 0 || totalPressure != 0 || profiles != 0)) {
+      float sensor_depth = sensor.depth();
+      if (sensor_depth >= 2 && sensor_depth <= 3) timeInRegion++;
+      depthQueue.push_back({millis(), sensor_depth, sensor.pressure()/10});
+    } else if (!profiling && (totalDepth != 0 || totalPressure != 0 || profiles != 0 || timeInRegion != 0)) {
       totalDepth = 0;
       totalPressure = 0;
       profiles = 0;
+      timeInRegion = 0;
     }
     vTaskDelay(1000 * portTICK_PERIOD_MS);
   }
@@ -102,14 +112,45 @@ void profile(void * parameter) {
   strcpy(message.text, "Profile commencing!");
   esp_now_send(broadcastAddress, (uint8_t *) &message, sizeof(message));
 
-  digitalWrite(IN1PIN, HIGH);
-  TickType_t startTick = xTaskGetTickCount();
-  while (digitalRead(BUTTONPIN) == 1) vTaskDelay(10 * portTICK_PERIOD_MS);
-  digitalWrite(IN2PIN, HIGH);
-  vTaskDelay(SINKTIME * portTICK_PERIOD_MS);
-  digitalWrite(IN1PIN, LOW);
-  vTaskDelay(TIMEOUT * portTICK_PERIOD_MS);
-  digitalWrite(IN2PIN, LOW);
+  while (digitalRead(BOTTOMPIN) == HIGH) {
+    ledcWrite(CHANNEL_IN1, 255);
+    ledcWrite(CHANNEL_IN2, 0);
+  }
+
+  while (timeInRegion < 45) {
+    // Serial.println(analogRead(POT_PIN));
+
+    float depth_val = sensor.depth();
+    int pwm = max(min((int) abs((2.5 - depth_val) * 150), 255), 175);
+
+    if (depth_val < 2.5 && digitalRead(BOTTOMPIN) == HIGH && analogRead(POT_PIN) > 1200) {
+      ledcWrite(CHANNEL_IN1, 255);
+      ledcWrite(CHANNEL_IN2, 0);
+    } else if (depth_val > 2.5 && digitalRead(TOPPIN) == HIGH && analogRead(POT_PIN) < 2350) {
+      ledcWrite(CHANNEL_IN1, 0);
+      ledcWrite(CHANNEL_IN2, 255);
+    } else {
+      ledcWrite(CHANNEL_IN1, 255);
+      ledcWrite(CHANNEL_IN2, 255);
+    }
+
+    vTaskDelay(100 * portTICK_RATE_MS);
+  }
+  
+  // You are done. Attempt to go back up
+
+  // To speed up going up, go to max buoyancy
+  while (digitalRead(TOPPIN) == HIGH) {
+    ledcWrite(CHANNEL_IN1, 0);
+    ledcWrite(CHANNEL_IN2, 255); // Full power up
+    vTaskDelay(100 * portTICK_PERIOD_MS);
+  }
+
+  // You're at max buoyancy, stop the motor
+  ledcWrite(CHANNEL_IN1, 0);
+  ledcWrite(CHANNEL_IN2, 0);
+
+  vTaskDelay(10000 * portTICK_PERIOD_MS);
 
   profiling = false;
   message.time = millis();
@@ -124,13 +165,12 @@ void prime(void * parameter) {
   strcpy(message.text, "Priming Profiler!");
   esp_now_send(broadcastAddress, (uint8_t *) &message, sizeof(message));
 
-  digitalWrite(IN1PIN, HIGH);
+  ledcWrite(CHANNEL_IN1, 0);
+  ledcWrite(CHANNEL_IN2, 255); // Full power expand
   TickType_t startTick = xTaskGetTickCount();
-  while (digitalRead(BUTTONPIN) == 1) vTaskDelay(10 * portTICK_PERIOD_MS);
-  digitalWrite(IN1PIN, LOW);
-  digitalWrite(IN2PIN, HIGH);
-  vTaskDelay(TIMEOUT * portTICK_PERIOD_MS);
-  digitalWrite(IN2PIN, LOW);
+  // Achieve minimum buoyancy state
+  while (digitalRead(TOPPIN) == 1) vTaskDelay(10 * portTICK_PERIOD_MS);
+  ledcWrite(CHANNEL_IN2, 0);
 
   priming = false;
   message.time = millis();
@@ -176,18 +216,22 @@ void setup() {
 
   sensor.read();
 
-  WiFi.mode(WIFI_AP_STA);
-  WiFi.softAP("EasternEdgeProfilerOTA", "crazyasspassword74");
-  WiFi.softAPConfig(IPAddress(192, 168, 0 , 1), IPAddress(192, 168, 0, 1), IPAddress(255, 255, 255, 0));
+  WiFi.mode(WIFI_STA);
 
   if (esp_now_init() != ESP_OK) {
     Serial.println("Unable to start ESP-NOW!");
     return;
   }
 
-  pinMode(BUTTONPIN, INPUT);
-  pinMode(IN1PIN, OUTPUT);
-  pinMode(IN2PIN, OUTPUT);
+  pinMode(POT_PIN, INPUT);
+  pinMode(BOTTOMPIN, INPUT);
+  pinMode(TOPPIN, INPUT);
+
+  ledcSetup(CHANNEL_IN1, PWM_FREQ, PWM_RES);
+  ledcAttachPin(IN1PIN, CHANNEL_IN1);
+  
+  ledcSetup(CHANNEL_IN2, PWM_FREQ, PWM_RES);
+  ledcAttachPin(IN2PIN, CHANNEL_IN2);
 
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
@@ -204,8 +248,8 @@ void setup() {
   strcpy(message.companyNumber, companyNumber);
   strcpy(depth.companyNumber, companyNumber);
 
-  ElegantOTA.begin(&server);
-  server.begin();
+  // ElegantOTA.begin(&server);
+  // server.begin();
 
   xTaskCreate(conCheck, "Connectivity Check", 1000, NULL, 1, NULL);
   xTaskCreate(getDepthData, "Get Depth Data", 1000, NULL, 1, NULL);
@@ -215,5 +259,6 @@ void setup() {
 
 void loop() {
   sensor.read();
-  ElegantOTA.loop();
+  sleep(0.05);
 }
+
--- ProfilerReciever/src/main.cpp
+++ ProfilerReciever/src/main.cpp
@@ -2,13 +2,10 @@
 #include <esp_now.h>
 #include <LittleFS.h>
 #include <ESPAsyncWebServer.h>
-#include <ArduinoJson.h>
-#include <rgb_lcd.h>
+#include <vector>
 
 AsyncWebServer server(80);
-String logs = "";
-
-rgb_lcd lcd;
+AsyncWebSocket ws("/ws");
 
 typedef struct struct_message {
   uint8_t mod = 1;
@@ -27,15 +24,18 @@ typedef struct struct_depth {
 } struct_depth;
 struct_depth depth;
 
-JsonDocument depthOverTime;
+struct DepthAndTime {
+  unsigned long time;
+  float depth;
+};
+
+std::vector<DepthAndTime> dataPoints;
 
 // A0:A3:B3:0F:7B:A0
 uint8_t broadcastAddress[] = {0xA0, 0xA3, 0xB3, 0x0F, 0x7B, 0xA0};
 esp_now_peer_info_t peerInfo;
 
 char tmp[250];
-
-int profileTime = 0;
 
 String zeroPrefix(int number) {return number < 10 ? "0" : "";}
 
@@ -49,7 +49,7 @@ String millisToTimeStr(const unsigned long time) {
 }
 
 void onSend(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  if (status != ESP_NOW_SEND_SUCCESS) logs += "Unable to send signal to profiler.\n";
+  if (status != ESP_NOW_SEND_SUCCESS) ws.textAll("Unable to send signal to profiler\n");
 }
 
 void onReceive(const uint8_t * mac, const uint8_t *incomingData, int len) {
@@ -58,23 +58,21 @@ void onReceive(const uint8_t * mac, const uint8_t *incomingData, int len) {
   switch(mod) {
     case(1):
       memcpy(&message, incomingData, sizeof(message));
-      sprintf(tmp, "[%s] - %s - %s\n", millisToTimeStr(message.time), message.companyNumber, message.text);
-      logs += tmp;
-      if (String(message.text) == "Profiler ready for deployment!") depthOverTime.clear();
-      if (String(message.text) == "Profile commencing!") {
-        // depthOverTime.clear();
-        profileTime = millis() - 1000;
-      }
-      if (String(message.text) == "Profile complete!") profileTime = 0;
+      sprintf(tmp, "[%s FLT] - %s - %s\n", millisToTimeStr(message.time), message.companyNumber, message.text);
+      ws.textAll(tmp);
+      // if (String(message.text) == "Profiler ready for deployment!" || String(message.text) == "Profile commencing!") dataPoints.clear();
       break;
     case(2):
       memcpy(&depth, incomingData, sizeof(depth));
       if(abs(depth.depth) < 10) {
-        depthOverTime[depthOverTime.size()][0] = (depth.time/1000);
-        depthOverTime[depthOverTime.size()-1][1] = depth.depth;
-        sprintf(tmp, "[%s] - %s - Depth: %f - Pressure: %f\n", millisToTimeStr(depth.time), depth.companyNumber, depth.depth, depth.pressure);
-        logs += tmp;
+        DepthAndTime tmp1;
+        tmp1.time = depth.time/1000;
+        tmp1.depth = depth.depth;
+        dataPoints.push_back(tmp1);
+        sprintf(tmp, "[%s FLT] - %s - Depth: %f m - Pressure: %f kPa\n", millisToTimeStr(depth.time), depth.companyNumber, depth.depth, depth.pressure);
+        ws.textAll(tmp);
       }
+      break;
     default:
       break;
   }
@@ -82,60 +80,64 @@ void onReceive(const uint8_t * mac, const uint8_t *incomingData, int len) {
 
 void setup() {
   Serial.begin(115200);
+
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAP("EasternEdgeStation", "crazyasspassword74");
   WiFi.softAPConfig(IPAddress(192, 168, 0 , 1), IPAddress(192, 168, 0, 1), IPAddress(255, 255, 255, 0));
 
   if (!LittleFS.begin()) {
-    Serial.println("Unable to start LittleFS!");
+    Serial.println("Failed to mount LittleFS");
     return;
   }
   if (esp_now_init() != ESP_OK) {
-    Serial.println("Unable to start ESP-NOW!");
+    Serial.println("Failed to start ESP-NOW!");
     return;
   }
 
-  esp_now_register_send_cb(onSend);
   esp_now_register_recv_cb(onReceive);
+  esp_now_register_send_cb(onSend);
 
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
-
-  lcd.begin(16, 2);
-  lcd.setRGB(69, 120, 120);
-  lcd.print("Float Time: 00:00:00");
 
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Unable to add peer!");
     return;
   }
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {request->send(LittleFS, "/index.html");});
-  server.on("/jquery.min.js", [](AsyncWebServerRequest *request) {request->send(LittleFS, "/jquery.min.js");});
-  server.on("/dygraph.min.js", [](AsyncWebServerRequest *request) {request->send(LittleFS, "/dygraph.min.js");});
-  server.on("/logs", [](AsyncWebServerRequest *request) {request->send_P(200, "text/plain", logs.c_str());});
+  ws.onEvent([](AsyncWebSocket *server, AsyncWebSocketClient *client,
+                AwsEventType type, void *arg, uint8_t *data, size_t len) {
+    if (type == WS_EVT_CONNECT) {
+      Serial.println("WebSocket client connected");
+    }
+  });
+
+  server.addHandler(&ws);
+  server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
+  server.serveStatic("/jquery.min.js", LittleFS, "/").setDefaultFile("jquery.min.js");
+  server.serveStatic("/dyngraph.min.js", LittleFS, "/").setDefaultFile("dyngraph.min.js");
   server.on("/data", [](AsyncWebServerRequest *request) {
-    String tmp;
-    serializeJson(depthOverTime, tmp);
-    request->send_P(200, "text/json", tmp.c_str());
+    AsyncResponseStream *response = request->beginResponseStream("application/json");
+    response->print("[");
+    for (size_t i = 0; i < dataPoints.size(); i++) {
+      response->print("[");
+      response->print(dataPoints[i].time);
+      response->print(",");
+      response->print(dataPoints[i].depth);
+      response->print("]");
+      if (i < dataPoints.size() - 1) response->print(",");
+    }
+    response->print("]");
+    request->send(response);
   });
   server.on("/start", [](AsyncWebServerRequest *request) {
     uint8_t startSignal = 1;
     esp_now_send(broadcastAddress, (uint8_t *) &startSignal, sizeof(uint8_t));
   });
-  
+
   server.begin();
 }
 
-void loop() {
-  //untested: no lcd screen
-  if (profileTime > 0) {
-    lcd.setCursor(0, 0);
-    lcd.print("Float Time: ");
-    lcd.print(millisToTimeStr(millis() - profileTime));
-    // Serial.println(millisToTimeStr(millis() - profileTime));
-  }
+void loop() {}
 
-  sleep(1);
-}
